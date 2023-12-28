e6e4c560cebedb493110bc2f52fac633e0f98f94
Franck Pedron <franckpedrondev@gmail.com>
2023-12-29 00:18:14 +0100

    Feat-Mise en place structure

---
+++ src/app/shared/components/backendErrorMessages/backendErrorMessages.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-backendErrorMessages',
+  standalone: true,
+  imports: [],
+  templateUrl: './backendErrorMessages.component.html',
+  styleUrl: './backendErrorMessages.component.scss'
+})
+export class BackendErrorMessagesComponent {
+
+}
---
+++ src/app/shared/components/popularTags/popularTags.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-popularTags',
+  standalone: true,
+  imports: [],
+  templateUrl: './popularTags.component.html',
+  styleUrl: './popularTags.component.scss'
+})
+export class PopularTagsComponent {
+
+}
